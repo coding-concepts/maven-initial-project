d44b3457b7012bb440d9f422be0c47df19975150
Pratyush Giri <pratyush.g@sparkcentral.com>
2018-07-09 16:19:17 -0700

    first commit.

---
+++ src/main/java/com/ccs/classes/maven/app/MainApplication.java
@@ -0,0 +1,8 @@
+package com.ccs.classes.maven.app;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("We are able to run the main class from a jar file.....");
+    }
+}
